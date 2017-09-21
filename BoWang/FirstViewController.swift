9d388277d32d0248f8292fe8f3d85fa1e87ca8ba
cocobear411 <727625206@qq.com>
2017-09-21 14:38:55 +1000

    1st update

---
+++ BoWang/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  TabBarApp
+//
+//  Created by 王博 on 18/9/17.
+//  Copyright © 2017 MelbUni. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
